ab2dc6604975267f7a4ac0cc00687e6856c6185e
aarochuk <andrewarochukwu@gmail.com>
2025-02-03 09:08:30 -0500

    fixed conflicts in styling between AddSkills and AddEducation page

--- src/AddSkill.js
+++ src/AddSkill.js
@@ -1,14 +1,14 @@
-import "./AddSkill.css";
+import styles from "./AddSkill.module.css";
 
 export default function AddEducation() {
   return(
     <div className="bodyContainer">
-      <div className="mainContainer">
+      <div className={styles.asMainContainer}>
         {/* Image of a logo of a school */}
         <h2>Add Skill</h2>
         {/*Image of an x to close and go back home */}
         <p>Add programming language, language, or framework you know include how proficient you are at the skill.</p>
-        <div className="formContainer">
+        <div className={styles.asFormContainer}>
           <input type="text" placeholder="Skill Name"/>
           <input type="text" placeholder="Skill Proficiency"/>
           <label>Skill Logo:</label>
